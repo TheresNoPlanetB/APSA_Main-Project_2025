a359046ad5963000c33eb1fdb5a3dc49a6925e69
brianjanas <brj24@pitt.edu>
2025-02-07 16:45:25 -0500

    Updated Circuit.py

--- Circuit.py
+++ Circuit.py
@@ -16,13 +16,13 @@ class Circuit:
             raise ValueError(f"Bus {name} already exists in the circuit.")
         self.buses[name] = Bus(name,base_kv)
 
-    def add_transformer(self, name, bus1, bus2, power_rating, impedance_percent, x_over_r_ratio):
+    def add_transformer(self, name, bus1, bus2, power_rating, impedance_percent, x_over_r_ratio, base_mva):
         # Adding transformer into circuit
         if name in self.transformer:
             raise ValueError(f"Transformer {name} already exists in the circuit.")
         if bus1 not in self.buses or bus2 not in self.buses:
             raise ValueError("Both buses must be added to the circuit before adding a transformer.")
-        self.transformer[name] = Transformer(name, self.buses[bus1], self.buses[bus2], power_rating, impedance_percent, x_over_r_ratio)
+        self.transformer[name] = Transformer(name, self.buses[bus1], self.buses[bus2], power_rating, impedance_percent, x_over_r_ratio, base_mva)
 
     def add_transmission_line(self, name, bus1, bus2, bundle, geometry, length):
         # Adding transmission line into circuit
@@ -30,7 +30,7 @@ class Circuit:
             raise ValueError(f"Transmission line {name} already exists in the circuit.")
         if bus1 not in self.buses or bus2 not in self.buses:
             raise ValueError("Both buses must be added to the circuit before adding a transmission line.")
-        self.transmission_line[name] = TransmissionLine(name, self.buses[bus1], self.buses[bus2], geometry, length)
+        self.transmission_line[name] = TransmissionLine(name, self.buses[bus1], self.buses[bus2], bundle, geometry, length)
 
     def __repr__(self):
         return (f"Circuit(name={self.name}, Buses={list(self.buses.keys())}, "
